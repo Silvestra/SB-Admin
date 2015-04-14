db8e67ad753b3f5bc9e9ef87fdce341706724632
Tadas <tadcka89@gmail.com>
2015-04-14 23:35:13 +0300

    Improve admin menu javascript.

--- js/sb-admin.js
+++ js/sb-admin.js
@@ -1,18 +1,36 @@
-$(function() {
-
-    $('#side-menu').metisMenu();
-
-});
-
-//Loads the correct sidebar on window load,
-//collapses the sidebar on window resize.
-$(function() {
-    $(window).bind("load resize", function() {
-        width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
-        if (width < 768) {
+$(function () {
+    $(window).bind("load resize", function () {
+        var $width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+        if ($width < 768) {
             $('div.sidebar-collapse').addClass('collapse')
         } else {
             $('div.sidebar-collapse').removeClass('collapse')
         }
-    })
+    });
+
+
+    var $adminMenu = $('#side-menu');
+    var setAdminActiveMenu = function () {
+        var $path = window.location.pathname;
+        $path = $path.replace(/\/$/, "");
+        $path = decodeURIComponent($path);
+
+        if ($path) {
+            $('#side-menu > li').each(function () {
+                var $href = $(this).find('a[href$="' + $path + '"]:first');
+                if ($href.length) {
+                    $(this).addClass('active');
+                    $(this).find('ul:first').addClass('in');
+                    $href.addClass('active');
+
+                    return true;
+                }
+            });
+        }
+    };
+
+    if ($adminMenu.length) {
+        $adminMenu.metisMenu();
+        setAdminActiveMenu();
+    }
 });
